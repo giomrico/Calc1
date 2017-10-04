eebcd03935e7fc4be47179f2d27f4ed16426a47f
ricog <ricog@wit.edu>
2017-10-04 03:11:06 -0400

    Added support for basic trig (sin and cos) and sqrt

--- src/Derivative.java
+++ src/Derivative.java
@@ -19,7 +19,6 @@ class Derivative {
     public static void main(String[] args) throws ScriptException {
         Scanner in = new Scanner(System.in);
         double start, stop;
-
         out.print("Please enter a output file location ");
             String fileOut = in.nextLine();
 
@@ -29,8 +28,8 @@ class Derivative {
             pw.close();
         } catch (FileNotFoundException e) {
             System.out.println("Output file not available");
+            System.exit(0);
         }
-        assert pw != null;
         pw.close();
 
         out.println("Please enter a function press enter for y=x : ");
@@ -72,15 +71,17 @@ class Derivative {
         while(start <= stop){
             double y = Double.parseDouble((Function(start+deltaX,function))) - Double.parseDouble((Function(start,function)));
             String func = Function(start, function);
+
                         /*This is the x value*/             /*This is value of y*/                              /*This is derivative*/
             String s = format.format(start) + "      " + format.format(Double.parseDouble(func)) + "        " + format.format((y/deltaX));
-            try (PrintWriter fout = new PrintWriter(new BufferedWriter(new FileWriter(fileOut, true)))) {
+
+            try (PrintWriter fOut = new PrintWriter(new BufferedWriter(new FileWriter(fileOut, true)))) {
                 if(i == 0){
                     System.out.println("x" + "          " + "f(x)" + "        " + "f'(x)");
-                    fout.println("x" + "            " + "f(x)" + "      " + "f'(x)");
+                    fOut.println("x" + "            " + "f(x)" + "      " + "f'(x)");
                     i = 1;
                 }else{
-                    fout.println(s);
+                    fOut.println(s);
                     System.out.println(s);
                 }
             }catch(Exception ex){
@@ -124,6 +125,7 @@ class Derivative {
                         base = j;
                     }else if (function.charAt(j) == '-'){
                         base = j;
+                        break;
                     }else{
                         break;
                     }
@@ -132,6 +134,36 @@ class Derivative {
                     if(Character.isDigit(function.charAt(k)) || function.charAt(k) == '.'){
                         power = k+1;
                     }else{
+                        break;
+                    }
+                }
+            }
+            if(function.charAt(i) == 's' && function.charAt(i+1) == 'i'){
+                for(int j = i; j < function.length(); j++ ){
+                    if(function.charAt(j) == ')'){
+                        //System.out.println(Double.toString(Math.sin(Double.parseDouble(function.substring(i+4,j)))) + "SIN VALUE");
+                        function = function.replace(function.substring(i,j+1),Double.toString(Math.sin(Double.parseDouble(function.substring(i+4,j)))));
+                        break;
+                    }
+                }
+            }
+            if(function.charAt(i) == 'c' && function.charAt(i+1) == 'o'){
+                for(int j = i; j < function.length(); j++ ){
+                    if(function.charAt(j) == ')'){
+                        //System.out.println(j + " " +Double.toString(Math.cos(Double.parseDouble(function.substring(i+4,j)))) + "COS VALUE");
+                        function = function.replace(function.substring(i,j+1),Double.toString(Math.cos(Double.parseDouble(function.substring(i+4,j)))));
+
+                        break;
+                    }
+                }
+            }
+            if(function.charAt(i) == 's' && function.charAt(i+1) == 'q'){
+                for(int j = i; j < function.length(); j++ ){
+                    if(function.charAt(j) == ')'){
+                        //System.out.println(j + " " +Double.toString(Math.cos(Double.parseDouble(function.substring(i+4,j)))) + "COS VALUE");
+                        //System.out.println((function.substring(i+5,j)));
+                        //System.exit(0);
+                        function = function.replace(function.substring(i,j+1),Double.toString(Math.sqrt(Double.parseDouble(Eval(function.substring(i+5,j))))));
                         break;
                     }
                 }
@@ -150,20 +182,39 @@ class Derivative {
      * Calculates the value of f(x) with a given function and x value
      *
      *
-     * @param x  
+     * @param x
      *          Value of x
      * @param function
      *          The function given by the user or default (y=x)
      * @return
      *          Returns the respected y value of f(x) in string form
      */
-    private static String Function(double x, String function) throws ScriptException {
+    private static String Function(double x, String function) {
         DecimalFormat df = new DecimalFormat("#.00000");
         x = Double.parseDouble(df.format(x));
         String function1 = functionStr(function, x);
         ScriptEngineManager evalFunction = new ScriptEngineManager();
         ScriptEngine evaluate = evalFunction.getEngineByName("js");
-        Object result = evaluate.eval(function1);
+        Object result = null;
+        try {
+            result = evaluate.eval(function1);
+        } catch (ScriptException e) {
+            System.out.println("Function not valid please ensure equation is correct " +
+                    "\n Ex: 2-(-x) != 2--x ; 2x^3+3/4 != ((2x^3)+3)/4");
+            System.exit(0);
+        }
+        return result.toString();
+
+    }
+    private static String Eval(String function){
+        ScriptEngineManager evalFunction = new ScriptEngineManager();
+        ScriptEngine evaluate = evalFunction.getEngineByName("js");
+        Object result = null;
+        try {
+            result = evaluate.eval(function);
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        }
         return result.toString();
     }
 
